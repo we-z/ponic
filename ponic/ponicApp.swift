37a23c320e325d6e626d443c13424e0dc7efa0fe
we-z <yousefsalem00@gmail.com>
2024-07-03 22:05:52 -0700

    Initial Commit

---
+++ ponic/ponicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ponicApp.swift
+//  ponic
+//
+//  Created by Wheezy Capowdis on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ponicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
