40d256a936402d4e5c75d8ac02e217c73d08c83a
we-z <yousefsalem00@gmail.com>
2024-07-04 11:31:20 -0700

    official services added

--- ponic/ContentView.swift
+++ ponic/ContentView.swift
@@ -77,26 +77,123 @@ struct ContentView: View {
                                 Color.clear.preference(key: ScrollViewOffsetKey.self,
                                     value: -$0.frame(in: .named("scrollView")).origin.y)
                             })
-                            
-                            ForEach(0..<10) { _ in
-                                HStack{
-                                    Image(systemName: "refrigerator.fill")
-                                        .font(.title)
-                                        .frame(width: 30)
-                                    Text("Testing")
-                                        .bold()
-                                    Spacer()
-                                    Image(systemName: "chevron.right")
-                                }
-                                .padding()
+                            HStack{
+                                Image(systemName: "refrigerator")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Controls")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
                             }
+                            .padding()
+                            HStack{
+                                Image(systemName: "fan")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Climate")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "laurel.leading")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Grow State")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "water.waves")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Water")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "alarm")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Schedule")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "checkmark.shield")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Safety")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "chart.bar")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Grow Stats")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "bag")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Shop")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "wrench.and.screwdriver")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Service")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
+                            HStack{
+                                Image(systemName: "exclamationmark.triangle")
+                                    .font(.title)
+                                    .frame(width: 30)
+                                Text("Alerts")
+                                    .bold()
+                                    .padding(.horizontal)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                            }
+                            .padding()
                         }
                     }
                     .coordinateSpace(name: "scrollView")
                     .onPreferenceChange(ScrollViewOffsetKey.self) { value in
                         headeOpacity = (460 - value) / 100
                         if value > 1 {
-                            p1Opacity = (100 / value) + 0.1
+                            p1Opacity = (30 / value)
                         } else {
                             p1Opacity = 1
                         }
